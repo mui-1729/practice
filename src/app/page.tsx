9ee86750ddfd6f791bdecd3028b66c2a94baffeb
mui-1729 <s1f102503015@iniad.org>
2025-05-13 23:52:34 +0900

    修繕

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 'use client';
-
+import { useEffect } from 'react';
 import styles from './page.module.css';
-
 export default function Home() {
   // 1からnまで足す
-  const n = prompt('入力してください');
-  const input: number = Number(n);
-  let sum = 0;
-  for (let i = 1; i <= input; i++) {
-    sum += i;
-  }
-  console.log(sum);
+  useEffect(() => {
+    const n = window.prompt('入力してください');
+    const input: number = Number(n);
+    let sum = 0;
+    for (let i = 1; i <= input; i++) {
+      sum += i;
+    }
+    console.log(sum);
+  }, []);
 
   // (s + sep) *3
   const repeatWithSep = (s: string, n: number, sep: string): string => {
@@ -23,15 +24,11 @@ export default function Home() {
   console.log(repeatWithSep('h', 3, '-'));
 
   const anyOver = (nums: number[], threshold: number): boolean => {
-    for (let i = 0; (i = nums.length); i++) {
-      if (i > threshold) {
-        return true;
-      }
-    }
-    return false;
+    return nums.some((num) => num > threshold);
   };
   console.log(anyOver([1, 5, 3], 4));
   console.log(anyOver([1, 2, 3], 4));
+
   return (
     <div className={styles.container}>
       <button> ボタン </button>
