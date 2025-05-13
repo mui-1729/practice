4c9d4b277dffb60476083ca5abbba026aa99ed41
mui-1729 <s1f102503015@iniad.org>
2025-05-13 23:15:14 +0900

    保存

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,57 +3,38 @@
 import styles from './page.module.css';
 
 export default function Home() {
+  // 1からnまで足す
+  const n = prompt('入力してください');
+  const input: number = Number(n);
+  let sum = 0;
+  for (let i = 1; i <= input; i++) {
+    sum += i;
+  }
+  console.log(sum);
+
+  // (s + sep) *3
+  const repeatWithSep = (s: string, n: number, sep: string): string => {
+    const parts: string[] = [];
+    for (let i = 1; i <= n; i++) {
+      parts.push(s);
+    }
+    return parts.join(sep);
+  };
+  console.log(repeatWithSep('h', 3, '-'));
+
+  const anyOver = (nums: number[], threshold: number): boolean => {
+    for (let i = 0; (i = nums.length); i++) {
+      if (i > threshold) {
+        return true;
+      }
+    }
+    return false;
+  };
+  console.log(anyOver([1, 5, 3], 4));
+  console.log(anyOver([1, 2, 3], 4));
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
-            src/app/page.tsx
-          </code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <button> ボタン </button>
     </div>
   );
 }
